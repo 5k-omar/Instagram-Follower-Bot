49746026adf92ef4b3f5a9d658e273c898300f9b
5k-omar <92945935+5k-omar@users.noreply.github.com>
2024-12-12 22:38:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import webbrowser
 
+login_url = "https://www.instagram.com/accounts/login/"
+url = "https://discord.gg/tcnksFMCR9"
+
+webbrowser.open(url)
+
+print("Join Discord Server For Use: https://discord.gg/tcnksFMCR9")
+
+input("Press any key to close...")
